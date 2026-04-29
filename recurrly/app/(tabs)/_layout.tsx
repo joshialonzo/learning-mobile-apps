6ca65a9144266c6a560294cdf1222cdfb33c57a3
Josué Alonzo-Chavarría <joshi.alonzo@gmail.com>
2026-04-29 01:42:58 -0600

    Recurrly: Tab Icons

--- recurrly/app/(tabs)/_layout.tsx
+++ recurrly/app/(tabs)/_layout.tsx
@@ -1,13 +1,44 @@
-import { Tabs } from "expo-router"
+import { tabs } from "@/constants/data";
+import { View } from "react-native";
+import { Image } from "expo-image";
+import { Tabs } from "expo-router";
+import clsx from "clsx";
 
-const TabLayout = () => (
-    <Tabs screenOptions={{ headerShown: false }}>
-        <Tabs.Screen name="index" options={{ title: "Home" }} />
-        <Tabs.Screen name="subscriptions" options={{ title: "Suscriptions" }} />
-        <Tabs.Screen name="insights" options={{ title: "Insights" }} />
-        <Tabs.Screen name="settings" options={{ title: "Settings" }} />
-        <Tabs.Screen name="subscriptions/[id]" options={{ href: null }} />
-    </Tabs>
-)
+const TabLayout = () => {
+    const TabIcon = ({ focused, icon}: TabIconProps) => {
+        return (
+            <View className="tabs-icon">
+                <View className={clsx("tabs-pill", focused && "tabs-active")}>
+                    <Image source={icon} className="tabs-glyph" />
+                </View>
+            </View>
+        );
+    };
+
+    return (
+        <Tabs screenOptions={{
+                headerShown: false,
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    position: "absolute",
+                }
+            }}>
+        {tabs.map((tab) => (
+            <Tabs.Screen
+                key={tab.name} 
+                name={tab.name} 
+                options={{ 
+                    title: tab.title,
+                    tabBarIcon: ({ focused }) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={tab.icon} />
+                    )
+                }}
+            />
+        ))}
+        </Tabs>
+    )
+}
 
 export default TabLayout;
