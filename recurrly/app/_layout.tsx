749be9c4be7fa8562f9f8a5f082906e8d3066b5d
Josué Alonzo <joshi.alonzo@gmail.com>
2026-04-29 19:23:30 -0600

    Recurrly: Custom Fonts
    * Recurrly: Custom Fonts
    
    * Recurrly: Add PlusJakartaSans-Medium.ttf to expo-font plugin config in
    
    * Recurrly: fix: call SplashScreen.preventAutoHideAsync() before font loading

--- recurrly/app/_layout.tsx
+++ recurrly/app/_layout.tsx
@@ -1,6 +1,28 @@
+import { useEffect } from "react";
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import "@/global.css";
 
+SplashScreen.preventAutoHideAsync().catch(console.warn);
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "sans-regular": require("@/assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "sans-bold": require("@/assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "sans-medium": require("@/assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "sans-semibold": require("@/assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    "sans-extrabold": require("@/assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "sans-light": require("@/assets/fonts/PlusJakartaSans-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if(fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return <Stack screenOptions={{ headerShown: false }}/>;
 }
